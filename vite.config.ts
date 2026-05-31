ada4363f9cc584663776a1742c1e9c3c8b9f7885
Zarya410 <pavelskotobazina@gmail.com>
2026-05-31 11:54:17 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/stardew-valley-calendar/',
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 900,
-  },
-});
+})
